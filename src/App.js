de77286b1e12ba2bd763c381eccf537cc205c1d5
Siddharth Gangwar <siddharth_gangwar@cargill.com>
2023-09-28 08:45:25 +0530

    fix: ui styles

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ const MpgInputBox = (props) => {
   const { mpgValue, setMpgValue, options } = props;
 
   return (
-    <div className='w-52 text-black'>
+    <div className={`w-52 text-black`}>
       <Listbox value={mpgValue} onChange={setMpgValue}>
         <div className='relative'>
           <Listbox.Button className='relative w-full cursor-default rounded-lg bg-blue-300/20 px-3 py-2 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
             <span className='block truncate'>{mpgValue.name}</span>
           </Listbox.Button>
           <Transition as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
-            <Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+            <Listbox.Options className='absolute z-30 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
               {options.map((person, personIdx) => (
                 <Listbox.Option
                   key={personIdx}
@@ -62,10 +62,11 @@ const MpgInputBox = (props) => {
 function InputBox(props) {
   const { value, setValue } = props;
   return (
-    <div className='flex px-3'>
+    <div className='flex pr-2'>
       <input
         type='number'
-        className='arrow-remove w-20 rounded-md border pl-2'
+        placeholder='Enter your value'
+        className='arrow-remove w-44 rounded-md border py-1 pl-2'
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
@@ -76,45 +77,65 @@ function InputBox(props) {
 function App() {
   const [mpgValue, setMpgValue] = useState(MPGValues[0]);
   const [headNeckMovement, setHeadNeckMovement] = useState(HeadNeckMovement[0]);
-  const [distance, setDistance] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
-  const [innerIncisorGap, setInnerIncisorGap] = useState(0);
+  const [distance, setDistance] = useState();
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
+  const [innerIncisorGap, setInnerIncisorGap] = useState();
   const [upperLipBite, setUpperLipBite] = useState(UpperLipBite[0]);
 
   const score = () => {
     let total = 0;
     total += parseInt(mpgValue.score) + parseInt(upperLipBite.score) + parseInt(headNeckMovement.score);
 
-    if (parseFloat(distance) > 6.5) {
-      total += 0;
-    } else if (parseFloat(distance) < 6) {
-      total += 2;
-    } else {
-      total += 1;
+    if (!isNaN(parseFloat(distance))) {
+      if (parseFloat(distance) > 6.5) {
+        total += 0;
+      } else if (parseFloat(distance) < 6) {
+        total += 2;
+      } else {
+        total += 1;
+      }
     }
 
-    if (parseFloat(distance) > 6.5) {
-      total += 0;
-    } else if (parseFloat(distance) < 6) {
-      total += 2;
-    } else {
-      total += 1;
+    if (!isNaN(parseFloat(innerIncisorGap))) {
+      if (parseFloat(innerIncisorGap) > 5) {
+        total += 0;
+      } else if (parseFloat(innerIncisorGap) < 4) {
+        total += 2;
+      } else {
+        total += 1;
+      }
     }
 
     return total;
   };
 
+  const getBMI = () => {
+    return parseFloat(parseFloat(weight) / parseFloat(height * height)).toFixed(2);
+  };
+
+  const getBMIColor = () => {
+    const bmi = getBMI();
+    if (bmi < 16) return "text-rose-700";
+    if (bmi < 17) return "text-rose-400";
+    if (bmi < 18.5) return "text-yellow-500";
+    if (bmi < 25) return "text-green-500";
+    if (bmi < 30) return "text-yellow-500";
+    if (bmi < 35) return "text-rose-400";
+    if (bmi < 40) return "text-rose-700";
+    return "text-rose-900";
+  };
+
   return (
-    <div className='flex h-screen w-screen flex-col items-center justify-center space-y-4 text-black'>
+    <div className='flex h-screen w-screen flex-col items-center justify-center space-y-4 bg-blue-50 text-black'>
+      <div className='pb-12 text-2xl font-bold sm:text-3xl md:text-5xl'>Department Of Anaesthesia</div>
       <div className='flex w-full items-center justify-start space-x-6 md:w-1/3'>
         <div className='flex w-1/3 items-center justify-start'>MPG: </div>
         <MpgInputBox mpgValue={mpgValue} setMpgValue={setMpgValue} options={MPGValues} />
       </div>
       <div className='flex w-full items-center justify-start space-x-6 md:w-1/3'>
         <div className='flex w-1/3 items-center justify-start'>Tm Distance: </div>
-        <InputBox value={distance} setValue={setDistance} />
-        cm
+        <InputBox value={distance} setValue={setDistance} /> cm
       </div>
       <div className='flex w-full items-center justify-start space-x-6 md:w-1/3'>
         <div className='flex w-1/3 items-center justify-start'>Head & Neck movement: </div>
@@ -128,7 +149,14 @@ function App() {
         <div className='flex w-1/3 items-center justify-start'>Height: </div>
         <InputBox value={height} setValue={setHeight} /> m
       </div>
-      <div className='text-blue-500 flex w-full md:w-1/3 items-center justify-start'>BMI: {parseFloat(weight) / parseFloat(height * height)}</div>
+      {!isNaN(getBMI()) ? (
+        <div className={`flex w-full items-center justify-start md:w-1/3`}>
+          <div className='flex w-1/3 items-center justify-start'>BMI: </div>
+          <div className={`flex pl-6 font-semibold ${getBMIColor()}`}>{getBMI()}</div>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className='flex w-full items-center justify-start space-x-6 md:w-1/3'>
         <div className='flex w-1/3 items-center justify-start'>Inner incisor gap: </div>
         <InputBox value={innerIncisorGap} setValue={setInnerIncisorGap} /> cm
@@ -137,7 +165,12 @@ function App() {
         <div className='flex w-1/3 items-center justify-start'>Upper lip Bite test: </div>
         <MpgInputBox mpgValue={upperLipBite} setMpgValue={setUpperLipBite} options={UpperLipBite} />
       </div>
-      <div className={` text-2xl pt-8 ${(score() < 3 ? "text-green-400" : score() < 8 ? "text-orange-500" : "text-rose-400")}`}>Score: {score()}</div>
+      <div className={`flex w-full items-center justify-start pt-8 text-3xl font-bold md:w-1/3`}>
+        <div className='flex w-1/3 items-center justify-start'>Score: </div>
+        <div className={`pl-6 ${score() < 3 ? "text-green-400" : score() < 8 ? "text-rose-400" : "text-rose-600"}`}>
+          {score()}
+        </div>
+      </div>
     </div>
   );
 }
